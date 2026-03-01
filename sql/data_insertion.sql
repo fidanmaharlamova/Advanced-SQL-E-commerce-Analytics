5098dda79bdebf1e97b1528f613a6069a9425732
Fidan Maharlamova <105640069+fidanmaharlamova@users.noreply.github.com>
2026-03-01 18:36:29 +0400

    Add files via upload

---
+++ sql/data_insertion.sql
@@ -0,0 +1,26 @@
+COPY public.customers 
+FROM 'C:\Users\Public\olist_customers_dataset.csv' 
+WITH (FORMAT CSV, HEADER);
+
+COPY public.orders
+FROM 'C:\Users\Public\olist_orders_dataset.csv' 
+WITH (FORMAT CSV, HEADER);
+
+COPY public.products 
+FROM 'C:\Users\Public\olist_products_dataset.csv' 
+WITH (FORMAT CSV, HEADER);
+
+COPY public.payments
+FROM 'C:\Users\Public\olist_order_payments_dataset.csv' 
+WITH (FORMAT CSV, HEADER);
+
+COPY public.sellers 
+FROM 'C:\Users\Public\olist_sellers_dataset.csv' 
+WITH (FORMAT CSV, HEADER);
+
+COPY public.order_items 
+FROM 'C:\Users\Public\olist_order_items_dataset.csv' 
+WITH (FORMAT CSV, HEADER);
+
+
+    
